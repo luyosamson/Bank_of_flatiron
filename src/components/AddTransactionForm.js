93bd1c83f4379eb57e925411dd6cae15eadddb65
luyosamson <luyosamson@gmail.com>
2022-09-25 20:14:21 +0300

    Post user's input onto the form

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React,{useState} from "react";
 
-function AddTransactionForm() {
+function AddTransactionForm({onSubmittion}) {
+
+  const [myFormData,setmyFormData]=useState(
+    {date:"",
+    description:"",
+    category:"",
+    amount: 0
+
+    })
+
+  function handleFormChange(e){
+    setmyFormData({...myFormData,[e.target.name]:e.target.value})
+
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    onSubmittion(myFormData)
+    // console.log(myFormData)
+
+  }
+
+
+
   return (
     <div className="ui segment">
-      <form className="ui form">
+      <form className="ui form" onChange={handleFormChange}   onSubmit={handleSubmit}  >
         <div className="inline fields">
-          <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
-          <input type="text" name="category" placeholder="Category" />
-          <input type="number" name="amount" placeholder="Amount" step="0.01" />
+        <label for="date">Date</label> 
+          <input type="date" name="date" value={myFormData.date} />
+          <input type="text" name="description" placeholder="Description" value={myFormData.description} />
+          <input type="text" name="category" placeholder="Category"  value={myFormData.category} />
+          <input type="number" name="amount" placeholder="Amount" step="0.01" value={myFormData.amount} />
         </div>
-        <button className="ui button" type="submit">
+        <button className="ui button" type="submit" >
           Add Transaction
         </button>
       </form>
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, {useEffect,useState} from "react";
-import AccountContainer from "./AccountContainer";
+// import AccountContainer from "./AccountContainer";
+import AddTransactionForm from "./AddTransactionForm";
 //import Transaction from "./Transaction";
 import TransactionsList from "./TransactionsList";
 
@@ -16,13 +17,23 @@ fetch(" http://localhost:8001/transactions")
    
   console.log(myTransactions)
 
+  function handleUpdateOnSubmission(myNewTransaction){
+    console.log(myNewTransaction)
+    
+    //Updating the form with user input
+    setMytransactions(myTransactions =>[...myTransactions,myNewTransaction])
+    
+
+  }
+
   return (
     <div className="ui raised segment">
       <div className="ui segment violet inverted">
         <h2>The Royal Bank of Flatiron</h2>
       </div>
-      <AccountContainer />
+      {/* <AccountContainer /> */}
       <div>
+        <AddTransactionForm onSubmittion={handleUpdateOnSubmission} />
         <TransactionsList transactions={myTransactions} />
       
       </div>
